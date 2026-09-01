e3f48c8fd323486200eaeedd1c9b571cfb2b311a
DhrudeepDesai <2023kucp1081@iiitkota.ac.in>
2026-09-01 20:48:38 +0530

    rmv

--- FRONTEND/src/lib/utils.js
+++
@@ -1,9 +0,0 @@
-// Date ko notes ke niche iss format mai "Aug 15, 2026" dikhana
-
-export function formatDate(date) {
-  return date.toLocaleDateString("en-US", {
-    month: "short",
-    day: "numeric",
-    year: "numeric",
-  });
-}
